ece56dcdd5a10fa0a8178b762c5e71e3f29f792a
Regreis Syziu <130826811+iamgreissy@users.noreply.github.com>
2023-11-22 08:24:53 +0100

    Add files via upload

---
+++ Contatto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace syziu.regreis.4i.rubrica
+{
+    internal class Contatto
+    {
+        private int _numero;
+        private string _cognome;
+
+        public int Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new ArgumentOutOfRangeException();
+
+                _numero = value;
+            }
+        }
+
+        public string Nome { get; set; }
+        public string EMail { get; set; }
+        public string Telefono { get; set; }
+        public string Citta { get; set; }
+        public string CAP { get; set; }
+
+        public string Cognome { get => _cognome; set => _cognome = value; }
+
+        public Contatto() { }
+
+        public Contatto(string riga)
+        {
+            string[] campi = riga.Split(';');
+            if (campi.Length >= 4)
+            {
+                if (int.TryParse(campi[0], out int numero))
+                {
+                    this.Numero = numero;
+                }
+
+                this.Nome = campi[1];
+                this.Cognome = campi[2];
+                this.Telefono = campi[3];
+                this.EMail = campi[4]; // Assumendo che l'email sia nella quinta colonna
+            }
+        }
+
+    public Contatto(int numero, string nome, string cognome)
+        {
+            Numero = numero;
+            Nome = nome;
+            Cognome = cognome;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            List<Contatto> rubrica = new List<Contatto>();
+
+            rubrica.Add(new Contatto(1, "Mario", "Rossi"));
+            rubrica.Add(new Contatto(2, "Luigi", "Verdi"));
+            rubrica.Add(new Contatto(3, "Anna", "Bianchi"));
+
+            Console.WriteLine("Rubrica:");
+            foreach (Contatto contatto in rubrica)
+            {
+                Console.WriteLine($"Numero: {contatto.Numero}, Nome: {contatto.Nome}, Cognome: {contatto.Cognome}");
+            }
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace syziu.regreis._4i.rubrica;
+    public partial class MainWindow : Window
+    {   
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Contatto[] Contatti = new Contatto[3];
+
+            try
+            {
+                Contatto c = new Contatto();
+                c.Numero = 1;
+                c.Nome = "Syziu";
+                c.Cognome = "Regreis";
+                c.EMail = "g.s04gg@gmail.com";
+                c.Telefono = "345678";
+                c.CAP = "47842";
+
+                Contatti[0] = c;
+
+                Contatto c1 = new Contatto { Numero = 2, Nome = "Giulia", Cognome = "Dumea" };
+                Contatti[1] = c1;
+
+                Contatto c2 = new Contatto(2, "Natasha", "Leone");
+                Contatti[2] = c2;
+
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("No no!!\n" + err.Message);
+            }
+
+        dgDati.ItemsSource = Contatti;
+
+        private void dgDati_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            Contatto contatto = e.Row.Item as Contatto;
+
+            if (contatto != null && (contatto.Numero < 0 || contatto.Numero > 100))
+            {
+                e.Row.Background = Brushes.Red;
+            }
+            else
+            {
+                e.Row.ClearValue(DataGridRow.BackgroundProperty);
+            }
+        }
+    }
+}
